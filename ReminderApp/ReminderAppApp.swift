d28b8d46306beda2fd21728a34bfe1d87b5e850d
jasminesung <jasminesung1130@gmail.com>
2024-04-16 21:07:30 -0400

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Jasmine Sung on 16/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
